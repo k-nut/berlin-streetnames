b938f8fe2f2a330471340ee31d4d9633a959cb57
Knut Hühne <knut@k-nut.eu>
2015-11-02 19:04:10 +0100

    Make map fullscreen, center it

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([52.505, 13.41], 11);
+var map = L.map('map').setView([52.52413, 13.44801], 14);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
